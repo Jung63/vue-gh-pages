8b4d25b58db2dab5e77e2eb67d945b1c0877cde3
Jung <kkaty51135tw@yahoo.com.tw>
2022-03-21 03:11:10 +0800

    del week6

--- week6/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <router-view/>
-  <!-- 往內層會展開FrontView 裡面有<navbar><router-view> -->
-</template>
-
-<style lang="scss">
-@import 'bootstrap/scss/bootstrap'
-</style>
--- week6/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/hexschool-Vue/week6/' //資料夾路徑，儲存庫名稱
-    : '/'
-}
